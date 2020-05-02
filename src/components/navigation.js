460eeae2f346ee25c7968fa80603a9607c6c649f
alicia-lu <4alicia.lu@gmail.com>
2020-05-02 12:28:33 +1200

    develop a simple gatsby website

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -11,6 +11,9 @@ export default () => (
       <li className={styles.navigationItem}>
         <Link to="/blog/">Blog</Link>
       </li>
+      <li className={styles.navigationItem}>
+        <Link to="/palindromes/">Palindromes</Link>
+      </li>
     </ul>
   </nav>
 )
